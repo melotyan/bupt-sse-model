24f5a574e6ff5e8de6274eaab9b4016afdb90a5f
hao.yan <hao.yan@dajie-inc.com>
2015-12-03 19:37:02 +0800

    改回@Data注解

--- src/main/java/cn/sse/bupt/model/UserModel.java
+++ src/main/java/cn/sse/bupt/model/UserModel.java
@@ -2,12 +2,13 @@ package cn.sse.bupt.model;
 
 
 import lombok.Data;
+
 import java.io.Serializable;
 
 /**
  * Created by hao.yan on 2015/12/1.
  */
-//@Data
+@Data
 public class UserModel implements Serializable {
 
     private long id;
@@ -18,68 +19,4 @@ public class UserModel implements Serializable {
     private String phone;
     private String address;
     private int userType;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public int getUserType() {
-        return userType;
-    }
-
-    public void setUserType(int userType) {
-        this.userType = userType;
-    }
 }
