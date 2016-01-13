c7d3b5436bbfa87043521535b2223748729a2cdc
hao.yan <hao.yan@dajie-inc.com>
2016-01-13 12:04:03 +0800

    添加意见实体类

---
+++ src/main/java/cn/sse/bupt/model/SuggestionModel.java
@@ -0,0 +1,22 @@
+package cn.sse.bupt.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by hao.yan on 2016/1/12.
+ */
+@Data
+public class SuggestionModel implements Serializable {
+    private static final long serialVersionUID = 4716000391797476616L;
+
+    private int id;
+    private int uid;
+    private String content;
+    private String title;
+    private Date createDate;
+    private Date updateDate;
+}
